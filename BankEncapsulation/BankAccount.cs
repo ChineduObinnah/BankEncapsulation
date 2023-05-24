27dea929e4d878deacc9ab097192ae9383604ead
Chinedu Obinnah~ <cobinnah@yahoo.com>
2023-05-24 09:43:42 -0500

    Bank Encap Complete

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double _balance = 0;
+
+        public void Deposit(double amountToAdd)
+        {
+            _balance += amountToAdd;
+        }
+        public double GetBalance() 
+        { 
+            return _balance; 
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+
+
+            Console.WriteLine($"How much do you want to deposit?");
+            double amountToAdd;
+            bool willParse = double.TryParse(Console.ReadLine(), out amountToAdd);
+            while (!willParse)
+            {
+                Console.WriteLine($"Invalid, Please Try Again.");
+                Console.WriteLine("Enter a valid amount:");
+                willParse = double.TryParse(Console.ReadLine(), out amountToAdd);
+
+
+            }
+            account.Deposit(amountToAdd);
+            Console.WriteLine($"Ammount Deposited!");
+
+            Console.WriteLine($"Do you want to see your balance?");
+            Console.WriteLine($"Please enter 'yes' or 'no'");
+
+            var cont = Console.ReadLine();
+            while (cont != "yes" && cont != "no")
+            {
+                Console.WriteLine($"Invalid please enter 'yes' or 'no'");
+                cont = Console.ReadLine();
+            }
+            while (cont == "yes")
+            {
+
+                switch (cont)
+                {
+                    case "yes":
+                        Console.WriteLine($"Current Balance {account.GetBalance()}");
+                        break;
+
+                    case "no":
+                        Console.WriteLine($"No Problem, Have a good day!");
+                        break;
+
+                }
+            }
+
         }
+
     }
 }
